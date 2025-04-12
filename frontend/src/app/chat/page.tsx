14d90e326daf30a26f94cc23718ab1dca3c276f6
potakaaa <helbirog@gmail.com>
2025-04-12 18:33:51 +0800

    added input container

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,7 +1,15 @@
-import { Cross, UserRound } from "lucide-react";
+import {
+  Cross,
+  Paperclip,
+  SendHorizontal,
+  Settings,
+  UserRound,
+} from "lucide-react";
 import React from "react";
 // import { Smiley, Heart, Horse } from "@phosphor-icons/react";
 import { HandWaving } from "@phosphor-icons/react/dist/ssr";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
@@ -26,23 +34,45 @@ const page = () => {
         </div>
       </section>
 
-      <section
-        id="welcome-message"
-        className="flex flex-col items-center gap-4 mt-30"
-      >
-        <span className="bg-foreground size-16 flex items-center justify-center rounded-2xl p-2 text-white shadow-lg">
-          <HandWaving fill="white" className="size-10 -scale-x-100" />
-        </span>
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h2 className="font-normal text-2xl">Hi, Student Nurse</h2>
-          <h2 className="font-extrabold text-2xl">
-            Can I help you with anything?
-          </h2>
-          <p className="font-light line-clamp-2 max-w-md text-sm">
-            Ready to assist you with anything student nurse-stuff related —
-            assignments, duties, or reflections. Let’s get started!
-          </p>
-        </div>
+      <section className="flex flex-col justify-between h-full items-center mt-30 flex-grow">
+        <section
+          id="welcome-message"
+          className="flex flex-col items-center gap-4"
+        >
+          <span className="bg-foreground size-16 flex items-center justify-center rounded-2xl p-2 text-white shadow-lg">
+            <HandWaving fill="white" className="size-10 -scale-x-100" />
+          </span>
+          <div className="flex flex-col items-center gap-1 text-center">
+            <h2 className="font-normal text-2xl">Hi, Student Nurse</h2>
+            <h2 className="font-extrabold text-2xl">
+              Can I help you with anything?
+            </h2>
+            <p className="font-light line-clamp-2 max-w-md text-sm">
+              Ready to assist you with anything student nurse-stuff related —
+              assignments, duties, or reflections. Let’s get started!
+            </p>
+          </div>
+        </section>
+        <section id="chat-container" className="">
+          <div
+            id="input-container"
+            className="w-[700px] flex flex-row gap-3 items-center justify-center"
+          >
+            <Button className="bg-foreground size-10 flex items-center justify-center rounded-full p-2 text-white shadow-lg cursor-pointer hover:bg-foreground/80 transition-all duration-200 ease-in-out">
+              <Settings className="size-5" />
+            </Button>
+            <section className="flex flex-row items-center justify-center bg-background rounded-full w-full h-12 px-3 py-2 shadow-lg">
+              <Button className="bg-[#BBBBBB] size-8 flex items-center justify-center rounded-full p-2 text-[#303030]/50 shadow-lg cursor-pointer hover:bg-[#BBBBBB]/80 transition-all duration-200 ease-in-out">
+                <Paperclip className="size-4" />
+              </Button>
+              <Input className="border-none shadow-none focus-visible:ring-0 rounded-full" />
+              <Button className="rounded-full bg-[#FFD292] text-foreground hover:bg-[#FFD292]/80 transition-all duration-200 ease-in-out cursor-pointer">
+                Send
+                <SendHorizontal className="size-4 ml-0 text-foreground" />
+              </Button>
+            </section>
+          </div>
+        </section>
       </section>
     </div>
   );
